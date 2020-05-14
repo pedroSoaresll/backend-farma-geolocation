a9163649d0ded94574ca2d970a7776d13f458f78
Pedro Oliveira <pedro.soliveira@stone.com.br>
2020-05-14 00:00:33 -0300

    feature: update mongo way connection + readd eslint + prettier + .gitignore

--- src/helper/mongo.ts
+++ src/helper/mongo.ts
@@ -2,31 +2,16 @@ import { ConnectionOptions } from 'mongoose'
 
 require('dotenv/config')
 
-const user = process.env.MONGO_USER ?? 'no-user'
-const password = process.env.MONGO_PASSWORD ?? 'no-password'
-const databaseName = process.env.MONGO_DATABASE
-const host = process.env.MONGO_HOST
-const port = process.env.MONGO_PORT
+const mongoURI = process.env.MONGO_URI
 
 export function getMongoURI(): string {
-  return `mongodb://${host}:${port}/${databaseName}`
+  return mongoURI
 }
 
 export function getMongoOptionConnection(): ConnectionOptions {
-  let options: ConnectionOptions = {
+  const options: ConnectionOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    authSource: 'admin',
-  }
-
-  if (process.env.NODE_ENV !== 'test') {
-    options = {
-      ...options,
-      auth: {
-        user,
-        password
-      }
-    }
   }
 
   return options
